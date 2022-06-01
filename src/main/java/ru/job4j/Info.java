f685dfa05c8e5863f91d200159955b8d666f34bf
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-01 19:18:35 +0300

    Info

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,5 +1,4 @@
 package ru.job4j;
-
 public class Info {
     public static void main(String[] args) {
         System.out.println("01.06.2022");
--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,7 +1,7 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         System.out.println("Yuliya Evdokimova");
         System.out.println("05.01.2001");
     }
