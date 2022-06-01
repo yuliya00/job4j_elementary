2d77fbbaabef25d3a472a159b776fbb87ec653c3
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-01 17:36:26 +0300

    StudentInfo

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main (String[] args) {
+        System.out.println("Yuliya Evdokimova");
+        System.out.println("05.01.2001");
+    }
+}
