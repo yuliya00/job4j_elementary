46f0234c48bd362f7f31d7a3c95ceb08e51241e5
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-06 16:55:51 +0300

    New

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -2,9 +2,7 @@ package ru.job4j.condition;
 
 public class Point {
     public static double distance(int x1, int y1, int x2, int y2) {
-        double first = Math.pow((x2 - x1), 2);
-        double two = Math.pow((y2 - y1), 2);
-        return Math.sqrt(first + two);
+        return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
     }
 
     public static void main(String[] args) {
