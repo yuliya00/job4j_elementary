ed2e7a7ec9d6e30d6d3f8989af4d2e36e63774a8
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-16 19:13:42 +0300

    Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,22 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        return switch (day) {
+            case 1 -> "Понедельник";
+            case 2 -> "Вторник";
+            case 3 -> "Среда";
+            case 4 -> "Четверг";
+            case 5 -> "Пятница";
+            case 6 -> "Суббота";
+            case 7 -> "Воскресенье";
+            default -> "Ошибка";
+        };
+    }
+
+    public static void main(String[] args) {
+        System.out.println(nameOfDay(5));
+        System.out.println(nameOfDay(2));
+        System.out.println(nameOfDay(10));
+    }
+}
