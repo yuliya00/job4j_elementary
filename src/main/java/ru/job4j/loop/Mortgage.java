d6ae3a95b0454c3a3f25119340e327be21deab8e
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-21 09:59:48 +0300

    Ипотека [#156315 #314486]

--- src/main/java/ru/job4j/loop/Mortgage.java
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,9 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(double amount, int salary, double percent) {
         int year = 0;
-        while (amount >= 0) {
-            amount = (amount + (amount * percent / 100)) - salary;
-            year += 1;
+        while (amount > 0) {
+            amount += amount * percent / 100 - salary;
+            year++;
         }
         return year;
     }
