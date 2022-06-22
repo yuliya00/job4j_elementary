f776ce41190c8cbba303d797215bc1311b1a84c4
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-22 11:18:28 +0300

    Перевернуть массив. [#226790 #315003]

--- src/main/java/ru/job4j/array/Turn.java
+++ src/main/java/ru/job4j/array/Turn.java
@@ -2,14 +2,10 @@ package ru.job4j.array;
 
 public class Turn {
     public static int[] back(int[] array) {
-        for (int index = 0; index < array.length; index++) {
-            int t = array[0];
-            array[0] = array[array.length - 1];
-            array[array.length - 1] = t;
-            int a = array[1];
-            array[1] = array[array.length - 2];
-            array[array.length - 2] = a;
-            return array;
+        for (int i = 0; i < array.length / 2; i++) {
+            int t = array[i];
+            array[i] = array[array.length - 1 - i];
+            array[array.length - 1 - i] = t;
         }
         return array;
     }
