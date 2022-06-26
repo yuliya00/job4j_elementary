b7977a0100bf12251f344379b6278bcf5fce1004
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-26 20:59:13 +0300

    SortSelected

--- src/main/java/ru/job4j/array/SortSelected.java
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -5,7 +5,7 @@ public class SortSelected {
         for (int i = 0; i < data.length; i++) {
             int min = MinDiapason.findMin(data, i, data.length - 1);
             int index = FindLoop.indexInRange(data, min, i, data.length - 1);
-            int[] a = SwitchArray.swap(data, index, i);
+            SwitchArray.swap(data, index, i);
         }
         return data;
     }
