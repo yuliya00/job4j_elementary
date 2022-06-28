4aad2ca169af746e08af8ece99591e7f4bbae7b2
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-28 19:40:47 +0300

    MatrixCheck2

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -3,8 +3,19 @@ package ru.job4j.array;
 public class MatrixCheck {
     public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (char i = 0; i < board[row].length; i++) {
+        for (char i = 0; i < board.length; i++) {
             if (board[row][i] == ' ') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+        for (char i = 0; i < board.length; i++) {
+            if (board[i][column] == ' ') {
                 result = false;
                 break;
             }
