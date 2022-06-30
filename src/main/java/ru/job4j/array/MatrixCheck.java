a24e91338c8a63beb62123f870a702075361bedd
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-30 17:29:45 +0300

    MatrixCheck1

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -15,7 +15,7 @@ public class MatrixCheck {
     public static boolean monoVertical(char[][] board, int column) {
         boolean result = true;
         for (char i = 0; i < board.length; i++) {
-            if (board[i][column] == ' ') {
+            if (board[i][column] != 'X') {
                 result = false;
                 break;
             }
--- src/test/java/ru/job4j/array/MatrixCheckTest.java
+++ src/test/java/ru/job4j/array/MatrixCheckTest.java
@@ -42,6 +42,18 @@ public class MatrixCheckTest {
     }
 
     @Test
+    public void whenNotHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', 'X', ' '},
+                {' ', ' ', 'X'},
+        };
+        int column = 2;
+        boolean result = MatrixCheck.monoVertical(input, column);
+        Assert.assertFalse(result);
+    }
+
+    @Test
     public void whenDiagonalFullX() {
         char[][] input = {
                 {'X', ' ', ' '},
