1c839841041a8c217d64ed8329bcd7df43294149
yuliya00 <yuliya.evdokimova.01.01@mail.ru>
2022-06-30 17:22:14 +0300

    Matrix

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -3,10 +3,9 @@ package ru.job4j.array;
 public class Matrix {
     public static int[][] multiple(int size) {
         int[][] data = new int[size][size];
-        for (int row = 0; row < data.length; row++) {
-            for (int cell = 0; cell < data[row].length; cell++) {
-                int a = (row + 1) * (cell + 1);
-                data[row][cell] = a;
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                data[row][cell] = (row + 1) * (cell + 1);
             }
         }
         return data;
